a1b95153281e80081fdc84caf0e65cc165a3a7ee
Jorge Luis Martinez Ricardo <jorgemartinezricardo@gmail.com>
2023-11-23 21:55:43 -0500

    Prueba Commit

---
+++ MiRepo/PrimerRepo.java
@@ -0,0 +1,6 @@
+package MiRepo;
+public class PrimerRepo {
+    public static void main(String[] args) {
+      System.out.println("Este es mi primer repo");
+    }
+}
---
+++ MiRepo/SegunRepo.py
@@ -0,0 +1,3 @@
+print(" Hola soy un repo")
+print("hola soy un cambio en el repo")
+print(" soy la segunda modificacion en el repo")
