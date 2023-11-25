7a544abd54da5e629b420703f4f1f71b72ed661b
Jorge Luis Martinez Ricardo <jorgemartinezricardo@gmail.com>
2023-11-24 23:11:14 -0500

    Archivos Modificados y mostrados con GIT DIFF

--- MiRepo/Ejemplo2.java
+++ MiRepo/Ejemplo2.java
@@ -1,5 +1,7 @@
+package MiRepo;
 public class Ejemplo2 {
     public static void main(String[] args) {
         System.out.println("Este es mi segundo Repo en java");
+        System.out.println("Modificando el archivo Ejemplo2.java");
     }
 }
