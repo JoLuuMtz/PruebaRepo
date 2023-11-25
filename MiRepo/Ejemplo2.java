5bbc3ad0ea06b10f19ff1024e133b3231cc6679f
Jorge Luis Martinez Ricardo <jorgemartinezricardo@gmail.com>
2023-11-25 10:36:08 -0500

    Modificaciones con Branch

--- MiRepo/Ejemplo2.java
+++ MiRepo/Ejemplo2.java
@@ -3,5 +3,6 @@ public class Ejemplo2 {
     public static void main(String[] args) {
         System.out.println("Este es mi segundo Repo en java");
         System.out.println("Modificando el archivo Ejemplo2.java");
+        System.out.println("se ha modificado este archivo para la rama no main");
     }
 }
